f35ffa3b808e78cd38aa61c9fdcb86061a51f068
Diesos <ozturk@live.Fr>
2024-05-20 23:26:01 +0200

    chore: Add initial files and components for React app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from './Nav'
+import MainContent from './MainContent'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Nav />
+    <MainContent />
+    </>
+  )
+}
+
+export default App
